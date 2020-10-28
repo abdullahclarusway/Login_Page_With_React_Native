4ea19e7d23bed22ccc436184780ab7be932b167f
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-10-28 01:00:23 +0100

    Login Page is created.

---
+++ index.js
@@ -0,0 +1,5 @@
+import 'react-native-gesture-handler';
+import {AppRegistry} from 'react-native';
+import Login from './src/Login'
+import {name as appName} from './app.json';
+AppRegistry.registerComponent(appName, () => Login);
